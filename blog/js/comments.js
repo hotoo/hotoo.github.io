e36b34275c11344938dc007c03a6d165cd8341a9
闲耘™ <hotoo.cn@gmail.com>
2010-09-17 18:50:41 +0800

    update for comment

--- blog/js/comments.js
+++ blog/js/comments.js
@@ -7,9 +7,14 @@
     for(var i=a.length-1; i>=0; i--){
         if(a[i].parentNode.tagName.toLowerCase()!="li"){continue;}
         var cmt=document.createElement("a");
-        cmt.class="comment-count";
+        cmt.className="comment-count";
         cmt.href=a[i].href+"#disqus_thread";
         cmt.appendChild(document.createTextNode("评论"));
-        a[i].parentNode.appendChild(cmt);
+        var span=document.createElement("span");
+        span.className="comment-count";
+        span.appendChild(document.createTextNode("("));
+        span.appendChild(cmt);
+        span.appendChild(document.createTextNode(")"));
+        a[i].parentNode.appendChild(span);
     }
 })();
