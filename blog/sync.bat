7e6a9faece9ab7dafa05fa7dad67430f0f6d6094
闲耘™ <hotoo.cn@gmail.com>
2010-10-21 17:06:13 +0800

    Automatic commit on 2010-10-21 17:06:12

--- blog/sync.bat
+++ blog/sync.bat
@@ -2,17 +2,16 @@
 title Sync Blogger...
 
 :PULL
-::Start...
-echo Start synchronizing...
+echo Blogger sync starting...
 git pull origin master
 
 
 :CONFIRM
-echo ""
-echo "======================================================================"
-set /p doPush=continue(y/n)?
+echo .
+echo ======================================================================
+set /p doPush=Pull blogger from server complete, continue to push(y/n)?
 if "%doPush%"=="y" goto PUSH
-if "%doPush%"=="n" goto DONE
+if "%doPush%"=="n" goto exit
 goto OONFIRM
 
 
@@ -26,17 +25,16 @@ set mydate=%DATE:~0,10%
 set mytime=%TIME:~0,8%
 set fvar=%mydate% %mytime%
 
-:: add all new files
-call git add .
-call git commit -a -m "Automatic commit on %fvar%"
+git add .
+git commit -a -m "Automatic commit on %fvar%"
 
 :: check if ssh-agent is running
 :: tasklist|find /i "ssh-agent.exe" || cmd /c ""C:\Program Files\Git\bin\sh.exe" --login -i" && exit
 call git push origin master
 
-echo ""
-echo "======================================================================"
-echo Synchronization is complete.
+echo .
+echo ======================================================================
+echo Sync is complete!
 
 :DONE
 pause
