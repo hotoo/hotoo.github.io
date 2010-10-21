3a5eacd38c87f5aa45c785484af9c08472e09c8a
闲耘™ <hotoo.cn@gmail.com>
2010-10-21 16:43:02 +0800

    Automatic commit on 2010-10-21 16:43:02

--- blog/sync.bat
+++ blog/sync.bat
@@ -1,22 +1,39 @@
+@echo off
 title Sync Blogger...
-::Start...
-echo Start synchronizing...
 
 :PULL
+::Start...
+echo Start synchronizing...
 git pull origin master
+@echo on
+
+
+:CONFIRM
+set /p doPush=continue(y/n)?
+if "%doPush%"=="y" goto PUSH
+if "%doPush%"=="n" goto DONE
+goto OONFIRM
+
 
 :PUSH
 echo Commit changes...
 
 :: get date and time
-for /f "delims=" %%a in ('date/t') do @set mydate=%%a
-for /f "delims=" %%a in ('time/t') do @set mytime=%%a
-set fvar=%mydate%%mytime%
+:: for /f "delims=" %%a in ('date/t') do @set mydate=%%a
+:: for /f "delims=" %%a in ('time/t') do @set mytime=%%a
+set mydate=%DATE:~0,10%
+set mytime=%TIME:~0,8%
+set fvar=%mydate% %mytime%
 
 :: add all new files
 call git add .
-call git commit -a -m "Automated commit on %fvar%"
+call git commit -a -m "Automatic commit on %fvar%"
 
 :: check if ssh-agent is running
-REM tasklist|find /i "ssh-agent.exe" || cmd /c ""C:\Program Files\Git\bin\sh.exe" --login -i" && exit
+:: tasklist|find /i "ssh-agent.exe" || cmd /c ""C:\Program Files\Git\bin\sh.exe" --login -i" && exit
 call git push origin master
+
+echo Synchronization is complete.
+
+:DONE
+pause
